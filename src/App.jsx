06ad1003b34328758479c065f25d7d9ca0341651
ayx <20233842+ayx234@users.noreply.github.com>
2025-10-13 17:40:55 +0300

    refactor(main.jsx): split App into components
    - Extract Screen and Buttons componenet from App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ export default function App() {
 	const [input, setInput] = useState("0");
 	const [display, setDisplay] = useState(0);
 
+	return (
+		<div
+			id="outer-container"
+			className="d-flex flex-column text-center bg-primary min-vh-100 fw-bold"
+		>
+			<h1 className="p-3 mb-0 bg-secondary">Javascript Calculator</h1>
+			<div
+				id="calculator-container"
+				className="flex-fill d-flex justify-content-center align-items-center bg-primary-subtle"
+			>
+				<div id="calculator" className="calculator bg-success">
+					<Screen input={input} display={display} />
+					<Buttons
+						input={input}
+						setInput={setInput}
+						setDisplay={setDisplay}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function Screen({ input, display }) {
+	return (
+		<div className="bg-warning calc-grid-col-4 text-end" id="screen">
+			<div id="input" className="input-display mx-3 fw-light">
+				{input}
+			</div>
+			<div className="mx-3 fs-3" id="display">
+				{display}
+			</div>
+		</div>
+	);
+}
+
+// Display helper functions
+
+function Buttons({ input, setInput, setDisplay }) {
 	// Add click events to buttons
 	useEffect(() => {
 		// Event handlers
@@ -36,98 +75,69 @@ export default function App() {
 		// Cleanup event listeners on App unmount
 		return () =>
 			buttons.forEach(b => b.removeEventListener("click", handleClick));
-	}, [input]);
+	}, [input, setInput, setDisplay]);
 
 	return (
-		<div
-			className="d-flex flex-column text-center bg-primary min-vh-100 fw-bold"
-			id="outer-container"
-		>
-			<h1 className="p-3 mb-0 bg-secondary">Javascript Calculator</h1>
-			<div
-				className="flex-fill d-flex justify-content-center align-items-center bg-primary-subtle"
-				id="calculator-container"
+		<>
+			<button id="clear" className="btn border rounded-0 calc-grid-col-2">
+				AC
+			</button>
+			<button id="divide" className="btn border rounded-0">
+				/
+			</button>
+			<button id="multiply" className="btn border rounded-0">
+				x
+			</button>
+			<button id="seven" className="btn border rounded-0">
+				7
+			</button>
+			<button id="eight" className="btn border rounded-0">
+				8
+			</button>
+			<button id="nine" className="btn border rounded-0">
+				9
+			</button>
+			<button id="subtract" className="btn border rounded-0">
+				-
+			</button>
+			<button id="four" className="btn border rounded-0">
+				4
+			</button>
+			<button id="five" className="btn border rounded-0">
+				5
+			</button>
+			<button id="six" className="btn border rounded-0">
+				6
+			</button>
+			<button id="add" className="btn border rounded-0">
+				+
+			</button>
+			<button id="one" className="btn border rounded-0">
+				1
+			</button>
+			<button id="two" className="btn border rounded-0">
+				2
+			</button>
+			<button id="three" className="btn border rounded-0">
+				3
+			</button>
+			<button
+				id="equals"
+				className="btn border rounded-0 calc-grid-row-2"
 			>
-				<div className="calculator bg-success" id="calculator">
-					<div
-						className="bg-warning calc-grid-col-4 text-end"
-						id="screen"
-					>
-						<div id="input" className="input-display mx-3 fw-light">
-							{input}
-						</div>
-						<div className="mx-3 fs-3" id="display">
-							{display}
-						</div>
-					</div>
-					<button
-						id="clear"
-						className="btn border rounded-0 calc-grid-col-2"
-						onClick={() => setDisplay(0)}
-					>
-						AC
-					</button>
-					<button id="divide" className="btn border rounded-0">
-						/
-					</button>
-					<button id="multiply" className="btn border rounded-0">
-						x
-					</button>
-					<button id="seven" className="btn border rounded-0">
-						7
-					</button>
-					<button id="eight" className="btn border rounded-0">
-						8
-					</button>
-					<button id="nine" className="btn border rounded-0">
-						9
-					</button>
-					<button id="subtract" className="btn border rounded-0">
-						-
-					</button>
-					<button id="four" className="btn border rounded-0">
-						4
-					</button>
-					<button id="five" className="btn border rounded-0">
-						5
-					</button>
-					<button id="six" className="btn border rounded-0">
-						6
-					</button>
-					<button id="add" className="btn border rounded-0">
-						+
-					</button>
-					<button id="one" className="btn border rounded-0">
-						1
-					</button>
-					<button id="two" className="btn border rounded-0">
-						2
-					</button>
-					<button id="three" className="btn border rounded-0">
-						3
-					</button>
-					<button
-						id="equals"
-						className="btn border rounded-0 calc-grid-row-2"
-					>
-						=
-					</button>
-					<button
-						id="zero"
-						className="btn border rounded-0 calc-grid-col-2"
-					>
-						0
-					</button>
-					<button id="decimal" className="btn border rounded-0">
-						.
-					</button>
-				</div>
-			</div>
-		</div>
+				=
+			</button>
+			<button id="zero" className="btn border rounded-0 calc-grid-col-2">
+				0
+			</button>
+			<button id="decimal" className="btn border rounded-0">
+				.
+			</button>
+		</>
 	);
 }
 
-// Helper functions
+// Buttons helper functions
 const cleanInput = (input, enteredChar) =>
 	new Promise(resolve => {
 		let result;
